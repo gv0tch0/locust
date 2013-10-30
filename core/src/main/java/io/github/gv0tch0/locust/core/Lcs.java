f9f986d987161a4e693db3a86a4371b199d75b27
Nik Kolev <gv0tch0@gmail.com>
2013-10-30 01:41:04 -0400

    Adds more specifics to the Lcs contract.

--- core/src/main/java/io/github/gv0tch0/locust/core/Lcs.java
+++ core/src/main/java/io/github/gv0tch0/locust/core/Lcs.java
@@ -9,13 +9,15 @@ import java.util.Set;
  */
 public interface Lcs {
     /**
-     * @param words The collection of words longest common substring is computed for.
-     * @return The longest common substring(s) for the given words. The returned
+     * @param words The collection of words longest common substrings are computed for.
+     * @return The longest common substrings for the given words. The returned
      *         {@link Set} contains more than one entry when there are more than one
-     *         distinct longest common substrings of the same length. When the given
+     *         distinct longest common substring of the same length. When the given
      *         words collection is empty the result is an empty set as well.
      *         When the given words collection contains a single element the result
-     *         is a copy of the given words collection.
+     *         is essentially a copy of the given words collection. When the members of
+     *         the words collection do not sport a common substring the result is an empty
+     *         set. White space is not treated differently (than non-white space).
      * @throws NullPointerException When the given words collection is <code>null</code>
      *         or when it contains one or more <code>null</code> words.
      */
